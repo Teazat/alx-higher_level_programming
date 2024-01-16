b54139d55072126947a09110ae2d50cef9c000de
Teazat <zac.thabang@gmail.com>
2024-01-16 06:17:24 -0800

    updates

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        last_digit = number % -(10)
-	print(-(last_digit), end='')
-    else:
-	last_digit = number % 10
-	print(last_digit, end='')
-    return abs(last_digit)
+#!/usr/bin/env python3
+print_last_digit = __import__('9-print_last_digit').print_last_digit
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
